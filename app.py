2b0597d6f19e8dbe0e0d0496774151a060cb9c5a
RACollins <richardcollins128@mail.com>
2023-12-02 11:11:37 +0900

    Added dither function

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ import altair as alt
 import numpy as np
 import pandas as pd
 
-# import cv2
-
 #################
 ### Functions ###
 #################
 
 
-def quantise_to_palette(img, palette):
+def reduce_colours(img, nColours):
+    image_pbn = img.convert("P", palette=Image.ADAPTIVE, colors=nColours)
+    return image_pbn
+
+
+def quantise_to_palette(img, palette, dither):
     """Quantize image to a given palette.
 
     The input image is expected to be a PIL Image.
@@ -31,14 +34,13 @@ def quantise_to_palette(img, palette):
     p.putpalette(palette)
 
     # Now quantize input image to the same palette as our little image
-    return img.convert("RGB").quantize(palette=p, dither=True)
+    return img.convert("RGB").quantize(palette=p, dither=dither)
 
 
 def get_palette_info(img):
     palette_colours = img.getcolors()
     imgRGB = img.convert("RGB")
     rgb_colours = imgRGB.getcolors()
-    # palette_info_dict = {c: [] for c in ["ColourID", "Frequency", "r", "g", "b"]}
     palette_info_dict = {c: [] for c in ["ColourID", "Frequency", "RGB"]}
     for palette_freq, palette_colour in palette_colours:
         for rgb_freq, rgb_colour in rgb_colours:
@@ -83,23 +85,35 @@ else:
     st.info("☝️ Upload a .jpg or .png file")
 
 
-with st.sidebar.form("sidebar_form"):
+with st.sidebar:
     st.header("Settings")
     colour_var_dict = {}
     nColours = st.number_input(
         "Number of colours", value=3, placeholder="Type a number..."
     )
+    img_pbn = reduce_colours(img=img_original, nColours=nColours)
+    img_pbn_info_df = get_palette_info(img_pbn)
+    default_colours = [rgb2hex(rgb) for rgb in img_pbn_info_df["RGB"].values]
     for c in range(int(nColours)):
-        colour_var_dict[c] = st.color_picker(f"Colour {c+1}", key=c)
+        colour_var_dict[c] = st.color_picker(
+            f"Colour {c+1}", key=c, value=default_colours[c]
+        )
+    with st.form("sidebar_form"):
+        st.subheader("Resize")
+        width = st.number_input(
+            "Width (pixels)", value=width, placeholder="Type a number..."
+        )
+        height = st.number_input(
+            "Height (pixels)", value=height, placeholder="Type a number..."
+        )
 
-    st.subheader("Resize")
-    width = st.number_input(
-        "Width (pixels)", value=width, placeholder="Type a number..."
-    )
-    height = st.number_input(
-        "Height (pixels)", value=height, placeholder="Type a number..."
-    )
-    palettise = st.form_submit_button("Palettise")
+        st.subheader("Dither")
+        isDither = st.checkbox(
+            "Apply dither",
+            help="With dither applied, random noise is added to the final image "
+            "which helps to smooth harsh edges.",
+        )
+        palettise = st.form_submit_button("Palettise")
 
 if palettise:
     img_resized = img_original.resize((width, height))
@@ -113,7 +127,9 @@ if palettise:
             ]
         ).flatten()
     )
-    img_new_palette = quantise_to_palette(img=img_resized, palette=new_palette)
+    img_new_palette = quantise_to_palette(
+        img=img_resized, palette=new_palette, dither=isDither
+    )
     st.text("Palettised Image")
     st.image(img_new_palette, use_column_width=True)
     img_new_palette_info_df = get_palette_info(img_new_palette)
